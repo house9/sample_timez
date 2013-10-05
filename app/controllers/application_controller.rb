a8de4c9532efb5636dd6a73ff61dd53092cbe10f
Jesse House <jesse.house@gmail.com>
2013-10-05 15:13:42 -0700

    set the timezone for each request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,25 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  before_filter :set_time_zone_for_user
+
+  after_filter :set_time_zone_for_system
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :time_zone
     devise_parameter_sanitizer.for(:account_update) << :time_zone
+  end
+
+  def set_time_zone_for_user
+    if current_user
+      Time.zone = current_user.time_zone
+    end
+  end
+
+  def set_time_zone_for_system
+    Time.zone = Rails.application.config.time_zone
   end
 
 end
