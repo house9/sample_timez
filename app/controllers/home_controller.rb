e90dcaebd2ba16e37e2766cecbebc7b8e4276838
Jesse House <jesse.house@gmail.com>
2013-11-02 12:55:22 -0700

    sql order vs ruby order in use_zone block

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @events_sql_order = Event.order(:start_at).includes(:organizer)
+    @events_ruby_order = Event.all.sort_by { |x| x.start_at }
   end
 end
