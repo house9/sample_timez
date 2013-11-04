14ebadf90084186eea6d6c36244bd985c14ae3d9
Jesse House <jesse.house@gmail.com>
2013-11-04 13:44:54 -0800

    work schedule ordered

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
   def index
-    # sorted data
+    # events: sorted data
     @events_sql_order = Event.order(:start_at).includes(:organizer)
     @events_ruby_order = Event.all.sort_by { |x| x.start_at }
 
-    # data from range
+    # events: data from range
     start_at = (Date.current - 1.day).beginning_of_day
     end_at = start_at.end_of_day
     # yesterday based on the current users time, but created_at is stored in UTC
@@ -13,5 +13,13 @@ class HomeController < ApplicationController
     user_time_zone = Time.zone.now.strftime("%Z")
     # postgres specific use ('timezone' or 'AT TIME ZONE') - http://www.postgresql.org/docs/9.3/static/functions-datetime.html#FUNCTIONS-DATETIME-ZONECONVERT
     @events_from_yesterday = Event.where("timezone(?, created_at) BETWEEN ? AND ?", user_time_zone, start_at, end_at)
+
+    start_at = (Date.current - 7.days).beginning_of_day
+    end_at = (start_at + 6.days).end_of_day
+    @events_from_last_week = Event.where("timezone(?, created_at) BETWEEN ? AND ?", user_time_zone, start_at, end_at)
+
+    # work schedules: sorted data
+    @work_schedule_sql_order = WorkSchedule.order(:start_at).includes(:worker)
+    @work_schedule_ruby_order = WorkSchedule.all.sort_by { |x| x.start_at }
   end
 end
