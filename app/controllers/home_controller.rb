2516a1f2af5929fc3bc6802ebe087f7fb8ac97ed
Jesse House <jesse.house@gmail.com>
2013-11-08 12:42:08 -0800

    add some counts to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,6 +18,15 @@ class HomeController < ApplicationController
     end_at = (start_at + 6.days).end_of_day
     @events_from_last_week = Event.where("timezone(?, created_at) BETWEEN ? AND ?", user_time_zone, start_at, end_at)
 
+    @event_count_upcoming = Event.where("start_at > ?", Time.zone.now).count
+    @event_count_upcoming_real = Event.where("timezone(?, start_at) > ?", user_time_zone, Time.zone.now).count
+    @event_count_total = Event.count
+
+    # logger.debug " ---------------------------"
+    # logger.debug Event.where("DATE(created_at) = ?", Date.current.yesterday).count
+    # logger.debug Event.where("timezone(?, created_at) BETWEEN ? AND ?", user_time_zone, start_at, end_at).count
+    # logger.debug " ---------------------------"
+
     # work schedules: sorted data
     @work_schedule_sql_order = WorkSchedule.order(:start_at).includes(:worker)
     @work_schedule_ruby_order = WorkSchedule.all.sort_by { |x| x.start_at }
